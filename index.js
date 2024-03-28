8b9506143e6abfaac2ddfbbb51311a01a1ca2b70
bravinkibetnyangusei <bravink599@gmail.com>
2024-03-28 16:05:31 +0300

    complete the lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(source, sought) {
+    return source.filter(function(possibleMatch) {
+        return possibleMatch.toLowerCase() === sought.toLowerCase();
+    });
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter(function(possibleMatch) {
+        return possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0;
+    });
+}
+
+function matchName(source, soughtName) {
+    return source.filter(function(record) {
+        return record.name === soughtName;
+    });
+}
